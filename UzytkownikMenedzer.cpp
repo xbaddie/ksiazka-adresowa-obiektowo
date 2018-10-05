cf67004ab2092631c75b26f6930478e6280f995e
ArtuR <artur@przyszlyprogramista.pl>
2018-10-05 14:35:44 +0200

    Added load user to vector on start program

--- UzytkownikMenedzer.cpp
+++ UzytkownikMenedzer.cpp
@@ -63,3 +63,8 @@ void UzytkownikMenedzer::wypiszWszystkichUzytkownikow()
         cout << uzytkownicy[i].pobierzHaslo() << endl;
     }
 }
+
+void UzytkownikMenedzer::wczytajUzytkownikowZPliku()
+{
+    uzytkownicy = plikZUzytkownikami.wczytajUzytkownikowZPliku();
+}
